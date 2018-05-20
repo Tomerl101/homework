ff7abd875989cef02c532098d90163e305240b38
Tomer <tomerl101@gmail.com>
2018-05-20 17:14:09 +0300

    added new style

--- ex3/includes/app.js
+++ ex3/includes/app.js
@@ -1,12 +1,9 @@
 var addBtn = document.getElementById('addBtn');
+var squares = document.getElementsByClassName('square');
 
 document.onload = (function(){
-    for(let i = 0; i<4 ; i++){
-        var newSquare = document.createElement("div");
-        newSquare.className = 'square';
-        newSquare.style.opacity = Math.random().toFixed(1);
-        document.getElementById('wrapper3').appendChild(newSquare);  
-    }
+    for(let i = 0; i<4 ; i++)
+        squares[i].style.opacity = Math.random().toFixed(1);  
 })();
 
 addBtn.onclick = function(){
